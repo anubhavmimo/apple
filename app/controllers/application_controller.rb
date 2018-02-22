9e4a63d850bc677ee2db733325fc1e05c627eaa2
Apple Ruby <aditymimo@gmail.com>
2018-02-22 03:40:20 -0500

    create rails application and setup home and about pages

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  def home
+  end
+  def about
+  end
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+ # protect_from_forgery with: :exception
+end
